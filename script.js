71326629b9dd192688d99b192e6eb969c66b8345
NMojsilovic <nikolamojsilovic03@gmail.com>
2026-03-04 11:48:39 +0100

    Animacije za skrolovanje

---
+++ script.js
@@ -0,0 +1,15 @@
+const items = document.querySelectorAll(".stavkaGrid");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, {
+  threshold: 0.3
+});
+
+items.forEach(item => {
+  observer.observe(item);
+});
